d7f21ff370091e37ac610438922fc893e4cca111
mwals003 <56177640+mwals003@users.noreply.github.com>
2019-10-04 19:56:02 -0700

    Updated main.c.

--- mwals003_lab2_part4/source/main.c
+++ mwals003_lab2_part4/source/main.c
@@ -1,23 +1,61 @@
-/*	Author: mwals003
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+/*	Author: Matthew Walsh (mwals003)
+ *  Partner(s) Name: None
+ *	Lab Section: 023
+ *	Assignment: Lab #2  Exercise #3
+ *	Exercise Description: Introduction to AVR Toolchain
  *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
+ *	I acknowledge all content contained herein, excluding
+ *  template or example code, is my own original work.
  */
+
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0x00; PORTB = 0xFF;
+	DDRC = 0x00; PORTC = 0xFF;
+	DDRD = 0xFF; PORTD = 0x00;
+	
+	unsigned char tmpA = 0x00;
+	unsigned char tmpB = 0x00;
+	unsigned char tmpC = 0x00;
+	unsigned char tmpD = 0x00;
+	
+	unsigned char total = 0x00;
+	unsigned char shift = 0x00;
 
-    /* Insert your solution below */
-    while (1) {
+	while(1) {
+	
+		tmpA = PORTA;
+		tmpB = PORTB;
+		tmpC = PORTC;
+        tmpD = 0x00;
+		
+		// Calculate total weight.
+		total = tmpA + tmpB + tmpC;
+		
+		// Calculate shifted weight.
+		if ((tmpA > tmpC) {
+			shift = tmpA - tmpC;
+        }
+		else if ((tmpA < tmpC) {
+            shift = tmpC - tmpA;
+        }
+        else {
+			shift = 0x00;
+		}
+		
+		// Determine if warning activates.
+		if ((total > 0x8C) || (shift > 0x50)) {
+			tmpD = 0x01;
+		}
+		
+		PORTD = tmpD;
+	}
+	
+	return 1;
 
-    }
-    return 1;
 }
